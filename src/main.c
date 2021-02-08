44af008f6b83405f2d0608d3a21887754d1626d9
Niyamaka <niyamaka@yuneta.io>
2021-02-08 20:25:24 +0100

    v4.8.9

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "logcenter"
 #define APP_DOC         "Log Center"
 
-#define APP_VERSION     "4.8.8"
+#define APP_VERSION     "4.8.9"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
