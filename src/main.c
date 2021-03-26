b08a3fe0cd063a8423fc88fe935363114cd4e7d6
Niyamaka <niyamaka@yuneta.io>
2021-03-26 09:09:36 +0100

    v4.10.16 fix openssl

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "logcenter"
 #define APP_DOC         "Log Center"
 
-#define APP_VERSION     "4.10.15"
+#define APP_VERSION     "4.10.16"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
